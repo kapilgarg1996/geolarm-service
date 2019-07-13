b4e2d3ce2b7293a2103ae0550666840debd2e470
kapilgarg1996 <kapilgarg1996@gmail.com>
2019-07-13 13:33:47 +0530

    Add basic logger views and urls

--- Dockerfile
+++ Dockerfile
@@ -2,7 +2,7 @@ FROM python:latest
 LABEL version=1.0.0
 WORKDIR /app
 RUN apt-get update
-ENV DATE 07-07-2019
+ENV DATE "07-07-2019 19:55"
 ADD ./requirements.txt /app
 RUN pip install -r requirements.txt
 EXPOSE 80
--- logger/views.py
+++ logger/views.py
@@ -1,6 +1,11 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
+from django.http import HttpResponse
 from django.shortcuts import render
+from geolarm_service import Get
 
 # Create your views here.
+
+def log(request):
+	return HttpResponse("Hello, I will log everything")
