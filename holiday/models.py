7683f2a9ca64b55c73785d0493f0ce6e73a711ed
kapilgarg1996 <kapilgarg1996@gmail.com>
2019-07-13 19:34:23 +0530

    Add holiday app to store holidays data

---
+++ holiday/models.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models
+
+# Create your models here.
+class HolidayCalendar(models.Model):
+	id = models.AutoField(primary_key=True)
+	date = models.DateField(db_index=True)
+	country = models.CharField(max_length=128)
+	description = models.CharField(max_length=256)
+
+	class Meta:
+		db_table = 'holidays'
+
+	@classmethod
+	def IsHoliday(cls, date, country):
+		holidays = cls.objects.filter(date=date, country=country).count()
+		if holidays >= 1:
+			return True
+		return False
